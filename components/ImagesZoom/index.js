8ac71391bd06f259b6723f78fab5381a39256f65
Envy <sungin0406@naver.com>
2023-09-26 01:37:36 +0900

    Feat #1 이미지, 이미지 카우셀 기능 구현

---
+++ components/ImagesZoom/index.js
@@ -0,0 +1,60 @@
+// 왜 폴더 안에 images.js를 만들었는가?
+// ImagesZoom.js로 바로 만들면 되지 않을까?
+// 스타일을 한 폴더에 정리해서 임포트 하면 코드가 더 깔끔해져서.
+// 구조화가 될 때 편하다.
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import Slick from "react-slick";
+import {
+  CloseBtn,
+  Global,
+  Header,
+  ImgWrapper,
+  Indicator,
+  Overlay,
+  SlickWrapper,
+} from "./styles";
+
+const ImagesZoom = ({ images, onClose }) => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  return (
+    <Overlay>
+      <Global />
+      <Header>
+        <h1>상세 이미지</h1>
+        <CloseBtn onClick={onClose}>X</CloseBtn>
+      </Header>
+      <SlickWrapper>
+        <div>
+          <Slick
+            initialSlide={0}
+            afterChange={(slide) => setCurrentSlide(slide)}
+            infinite
+            arrows={false}
+            slidesToShow={1}
+            slidesToScroll={1}
+          >
+            {images.map((v) => (
+              <ImgWrapper key={v.src}>
+                <img src={v.src} alt={v.src} />
+              </ImgWrapper>
+            ))}
+          </Slick>
+          <Indicator>
+            <div>
+              {currentSlide + 1} / {images.length}
+            </div>
+          </Indicator>
+        </div>
+      </SlickWrapper>
+    </Overlay>
+  );
+};
+
+ImagesZoom.propTypes = {
+  images: PropTypes.arrayOf(PropTypes.object).isRequired,
+  onClose: PropTypes.func.isRequired,
+};
+
+export default ImagesZoom;
--- components/PostImages.js
+++ components/PostImages.js
@@ -1,8 +1,85 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import PropTypes from "prop-types";
+import { PlusOutlined } from "@ant-design/icons";
+import styled from "styled-components";
+import ImagesZoom from "./ImagesZoom";
+
+const ImageWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ButtonWrapper = styled.div`
+  display: inline-block;
+  width: 50%;
+  text-align: center;
+  vertical-align: middle;
+`;
 
 const PostImages = ({ images }) => {
-  return <div>구현 중...</div>;
+  const [showImagesZoom, setShowImagesZoom] = useState(false);
+  const onZoom = useCallback(() => {
+    setShowImagesZoom(true);
+  }, []);
+  const onClose = useCallback(() => {
+    setShowImagesZoom(false);
+  }, []);
+  if (images.length === 1) {
+    return (
+      <>
+        <img
+          role="presentation" // 시각장애인용으로, 스크린 리더에서 클릭할 순 있지만 굳이 클릭하지 않아도 됨을 알림
+          src={images[0].src}
+          alt={images[0].src}
+          onClick={onZoom}
+        />
+        {showImagesZoom && <ImagesZoom images={images} onClose={onClose} />}
+      </>
+    );
+  }
+  if (images.length === 2) {
+    return (
+      <>
+        <img
+          role="presentation"
+          style={{ display: "inline-block", width: "50%" }}
+          src={images[0].src}
+          alt={images[0].src}
+          onClick={onZoom}
+        />
+        <img
+          role="presentation"
+          style={{ display: "inline-block", width: "50%" }}
+          src={images[1].src}
+          alt={images[1].src}
+          onClick={onZoom}
+        />
+        {showImagesZoom && <ImagesZoom images={images} onClose={onClose} />}
+      </>
+    );
+  }
+  return (
+    //현재 좌우 디자인 높이가 맞지 않는 이슈가 있음.
+    //flex를 사용해서 디자인을 적용시켜보려 했으나 아예 styled-components가 적용되지 않는 이슈 발생
+    <ImageWrapper>
+      <div>
+        <img
+          role="presentation"
+          style={{ display: "inline-block", width: "50%" }}
+          src={images[0].src}
+          alt={images[0].src}
+          onClick={onZoom}
+        />
+        <ButtonWrapper role="presentation" onClick={onZoom}>
+          <PlusOutlined />
+          <br />
+          {images.length - 1}개의 사진 더 보기
+        </ButtonWrapper>
+      </div>
+      {showImagesZoom && <ImagesZoom images={images} onClose={onClose} />}
+    </ImageWrapper>
+  );
 };
 
 PostImages.propTypes = {
