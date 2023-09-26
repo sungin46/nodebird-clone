b7d7bce43c2cc795e79a855d2aa67bfed0f2b0e1
Envy <sungin0406@naver.com>
2023-09-26 19:29:42 +0900

    Feat #1 styled-components 새로고침 오류 및 반응형 수정

--- components/PostImages.js
+++ components/PostImages.js
@@ -8,6 +8,16 @@ const ImageWrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  @media only screen and (max-width: 576px) {
+    flex-direction: column;
+  }
+`;
+const ImageMoreTwo = styled.img`
+  display: inline-block;
+  width: 50%;
+  @media screen and (max-width: 576px) {
+    width: 100%;
+  }
 `;
 
 const ButtonWrapper = styled.div`
@@ -41,16 +51,14 @@ const PostImages = ({ images }) => {
   if (images.length === 2) {
     return (
       <>
-        <img
+        <ImageMoreTwo
           role="presentation"
-          style={{ display: "inline-block", width: "50%" }}
           src={images[0].src}
           alt={images[0].src}
           onClick={onZoom}
         />
-        <img
+        <ImageMoreTwo
           role="presentation"
-          style={{ display: "inline-block", width: "50%" }}
           src={images[1].src}
           alt={images[1].src}
           onClick={onZoom}
@@ -62,11 +70,10 @@ const PostImages = ({ images }) => {
   return (
     //현재 좌우 디자인 높이가 맞지 않는 이슈가 있음.
     //flex를 사용해서 디자인을 적용시켜보려 했으나 아예 styled-components가 적용되지 않는 이슈 발생
-    <ImageWrapper>
-      <div>
-        <img
+    <div>
+      <ImageWrapper>
+        <ImageMoreTwo
           role="presentation"
-          style={{ display: "inline-block", width: "50%" }}
           src={images[0].src}
           alt={images[0].src}
           onClick={onZoom}
@@ -76,9 +83,9 @@ const PostImages = ({ images }) => {
           <br />
           {images.length - 1}개의 사진 더 보기
         </ButtonWrapper>
-      </div>
+      </ImageWrapper>
       {showImagesZoom && <ImagesZoom images={images} onClose={onClose} />}
-    </ImageWrapper>
+    </div>
   );
 };
 
